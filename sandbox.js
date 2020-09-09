8a176254353f4d34232084ef34f2e5826b50c590
SIA TestDevLab <siatestdevlab@school.local>
2020-09-09 09:14:43 +0300

    home

--- sandbox.js
+++ sandbox.js
@@ -136,10 +136,24 @@ let object =  {
     a: 1,
     b: 2
 }
+// let toArray = (object) => {
+//     let array = [];
+//     for (let key in object) {
+//         let temp = {};
+//         temp[key] = object[key];
+//         array.push(temp)
+//     }
+//     return array;
+// }
+
+// let showArray = (array) => array.forEach(element => console.log(element));
+
+// showArray(toArray(object));
+
 let toArray = (object) => {
     let array = [];
     for (let key in object) {
-        let temp = {};
+        let temp = [];
         temp[key] = object[key];
         array.push(temp)
     }
